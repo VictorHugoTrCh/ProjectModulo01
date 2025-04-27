cd8fe82c7d710306533e406a1324a44dd8a7e379
Victor Hugo Trujillo Charalla <htrujillocharalla@gmail.com>
2025-04-26 21:33:43 -0500

    agregar la orden en un archivo.txt

--- 01-shopping-cart/main.py
+++ 01-shopping-cart/main.py
@@ -1,5 +1,7 @@
 import os
 
+FILE_NAME = 'ventas.txt'
+
 WAREHOUSE = [
 
     {
@@ -18,7 +20,7 @@ WAREHOUSE = [
 # Código: A001 | Producto: Pan | Precio: S/1.50
 # Código: B203 | Producto: Leche | Precio: S/3.80
 
-SHOPPING_CAR = []
+SHOPPING_CART = []
 
 
 def show_menu():
@@ -35,13 +37,13 @@ def show_menu():
 def show_catalog():
     print(f"Código  |  Producto  |  Precio")
     for product in WAREHOUSE:
-        print(f" {product["code"]} | {product["name"]}{" "*(20-len(product["name"]))} | {product["price"]}")
+        print(f" {product["code"]} | {product["name"]}{" "*(10-len(product["name"]))} | {product["price"]}")
 
 
 def add_product_to_shopping_car(code_product):
     for product in WAREHOUSE:
         if code_product == product["code"]:
-            SHOPPING_CAR.append(product)
+            SHOPPING_CART.append(product)
             print(f"✔ El Producto {product["code"]} fue agregado al carrito.")
             break
 
@@ -49,5 +51,40 @@ def add_product_to_shopping_car(code_product):
 
 # show_menu()
 # show_catalog()
-os.system("clear")
-add_product_to_shopping_car("A002")
+# os.system("clear")
+# add_product_to_shopping_car("A002")
+
+
+def add_order_to_file(registerd_at, list_products, total_price):
+    complete_order = "=============================\n\n"
+    complete_order += f"Fecha: {registerd_at}\n\n"
+    
+    for prod in list_products:
+        complete_order += f"{prod['quantity']}  |  {prod['name']}  |  S/{prod['price']:.2f}  |  S/{prod['price'] * prod['quantity']:.2f}\n"
+
+
+
+    complete_order += f"S/. {total_price}\n\n"
+    complete_order += "=============================\n"
+
+
+    with open(FILE_NAME,'a') as archivo:
+        archivo.write(complete_order)
+
+
+add_order_to_file(
+    "20-04-2025 10:56:50",
+    [{
+        "quantity": 1,
+        "code":"A001",
+        "name":"Pan",
+        "price":1.5
+    },
+    {
+        "quantity": 1,
+        "code":"A002",
+        "name":"Leche",
+        "price":2.5
+    }],
+    4.0
+    )
